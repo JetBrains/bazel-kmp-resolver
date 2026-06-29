66ab59a2d2a1a6ec03255eade6648a0e198d6191
Titouan Bion <titouan.bion@jetbrains.com>
2026-06-29 15:42:51 +0200

    Refactor tests

--- test/org/jetbrains/kmp/resolver/MultiplatformResolverTest.kt
+++ test/org/jetbrains/kmp/resolver/MultiplatformResolverTest.kt
@@ -11,7 +11,7 @@ class MultiplatformResolverTest {
     // TODO: would be nice to mock Maven repositories to avoid real HTTP calls
     @OptIn(ExperimentalSerializationApi::class)
     @Test
-    fun `resolution with substitutions and multiple repositories`() = runBlocking {
+    fun `multi-repository resolution`() = runBlocking {
         val cache = createTempDirectory("simple")
         val repositories = listOf(
             "https://repo1.maven.org/maven2",
@@ -21,7 +21,72 @@ class MultiplatformResolverTest {
             "io.ktor:ktor-client-cio:3.5.0",
             "io.ktor:ktor-client-core:3.4.3",
             "org.jetbrains.kotlin:kotlin-reflect:2.4.0",
-            "org.jetbrains.intellij.deps.kotlinx:kotlinx-coroutines-core:1.10.2-intellij-1",
+        )
+        val artifactResolver = ArtifactUrlResolver(
+            allowedConcurrentConnections = 100,
+            connectTimeout = 30.seconds,
+            requestTimeout = 30.seconds,
+        )
+        val actual = artifactResolver.use { artifactResolver ->
+            val resolver = MultiplatformResolver(
+                cachePath = cache,
+                repositories = repositories.map { MavenRepository(it) },
+                artifactResolver = artifactResolver,
+                substitutions = emptyMap()
+            )
+            resolver.resolve(coordinates)
+        }
+
+        assertUsingManifest(
+            coordinates = coordinates,
+            repositories = repositories,
+            libraries = actual,
+            manifestResourceFilepath = "manifest-ktor_resolution.json",
+        )
+    }
+
+    @OptIn(ExperimentalSerializationApi::class)
+    @Test
+    fun `do not resolve JVM nodes`() = runBlocking {
+        val cache = createTempDirectory("simple")
+        val repositories = listOf(
+            "https://repo1.maven.org/maven2",
+        )
+        val coordinates = listOf(
+            "org.jetbrains.kotlin:kotlin-reflect:2.4.0",
+        )
+        val artifactResolver = ArtifactUrlResolver(
+            allowedConcurrentConnections = 100,
+            connectTimeout = 30.seconds,
+            requestTimeout = 30.seconds,
+        )
+        val actual = artifactResolver.use { artifactResolver ->
+            val resolver = MultiplatformResolver(
+                cachePath = cache,
+                repositories = repositories.map { MavenRepository(it) },
+                artifactResolver = artifactResolver,
+                substitutions = emptyMap()
+            )
+            resolver.resolve(coordinates)
+        }
+
+        assertUsingManifest(
+            coordinates = coordinates,
+            repositories = repositories,
+            libraries = actual,
+            manifestResourceFilepath = "manifest-no_jvm.json",
+        )
+    }
+
+    @OptIn(ExperimentalSerializationApi::class)
+    @Test
+    fun `substituted resolution`() = runBlocking {
+        val cache = createTempDirectory("simple")
+        val repositories = listOf(
+            "https://repo1.maven.org/maven2",
+        )
+        val coordinates = listOf(
+            "io.ktor:ktor-client-core:3.4.3",
         )
         val artifactResolver = ArtifactUrlResolver(
             allowedConcurrentConnections = 100,
@@ -45,7 +110,7 @@ class MultiplatformResolverTest {
             coordinates = coordinates,
             repositories = repositories,
             libraries = actual,
-            manifestResourceFilepath = "manifest-ktor_resolution.json",
+            manifestResourceFilepath = "manifest-with_substitutions.json",
         )
     }
 }
